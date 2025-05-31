7a4a4e9f6c005b6b595c786563b3f32adbe4752f
Yasith <141606159+yasith-1@users.noreply.github.com>
2025-05-31 06:30:19 +0530

    add attributes to PostEntity.java class & connect lombok

--- src/main/java/edu/icet/blogpostApp/entity/PostEntity.java
+++ src/main/java/edu/icet/blogpostApp/entity/PostEntity.java
@@ -1,4 +1,21 @@
 package edu.icet.blogpostApp.entity;
 
+import lombok.*;
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class PostEntity {
+    private Integer id;
+    private String title;
+    private String content;
+    private String tags;
+    private String category;
+    private Integer commentCount;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+    private String imageUrl;
 }
